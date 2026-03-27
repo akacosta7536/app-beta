0971b525bfd1d916caed1421accc42be8c015982
Kathe-a <kathe.acosta05@gmail.com>
2026-03-27 11:25:54 -0500

    Validacion de email unico

--- src/signup.php
+++ src/signup.php
@@ -9,6 +9,15 @@ $m_phone = $_POST['mphone'];
 $p_sswd = $_POST['passwd'];
 $enc_pass = md5($p_sswd);
 
+//Validar email existente
+$check_email = "SELECT * FROM users WHERE email = '$e_mail'";
+$result_email = pg_query($check_email);
+
+if (pg_num_rows($result_email) > 0) {
+    echo "El email ya está registrado";
+    exit;
+}
+
 //Query to insert in to SQL
 $sql = "INSERT INTO users (firstname, lastname, email, mobile_phone, password)
  VALUES ('$f_name','$l_name', '$e_mail','$m_phone','$enc_pass')";
